303022a3cfd41b0a9eb917e6a422e87d27903ba4
MdNahidDev <mdnahidhasan.tech@gmail.com>
2025-08-29 17:34:48 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// Emergency services data
+const services = [
+  { name: "National Emergency Number", subtitle: "National Emergency", number: "999", icon: "emergency.png" },
+  { name: "Police Helpline Number", subtitle: "Police", number: "999", icon: "police.png" },
+  { name: "Fire Service Number", subtitle: "Fire Service", number: "999", icon: "fire-service.png" },
+  { name: "Ambulance Service", subtitle: "Ambulance Service", number: "1994-999999", icon: "ambulance.png" },
+  { name: "Anti-Corruption Helpline", subtitle: "Anti-Corruption", number: "106", icon: "emergency.png" },
+  { name: "Women & Child Helpliner", subtitle: "Women & Child Helpline", number: "109", icon: "emergency.png" },
+  { name: "Electricity Helpline", subtitle: "Electricity Outage", number: "16216", icon: "emergency.png" },
+  { name: "Brac Helpline", subtitle: "Brac", number: "16445", icon: "brac.png" },
+  { name: "Bangladesh Railway Helpline", subtitle: "Bangladesh Railways", number: "163", icon: "Bangladesh-Railway.png" }
+];
